312d49b27e9606d8c34b050ecc7062a5bf976458
Adnan Aziz <adnan.832@gmail.com>
2021-01-21 23:23:55 +0100

    deleted file

--- javascript/javascript1/week1/Age-ify.js
+++
@@ -1,5 +0,0 @@
-//Age-ify (A future age calculator)
-var yearOfBirth = 1985;
-let yearFuture = 2027;
-const age = yearFuture - yearOfBirth;
-console.log ("You will be " + age + " years old in " + yearFuture);
