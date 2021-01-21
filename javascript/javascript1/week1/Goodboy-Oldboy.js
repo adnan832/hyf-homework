4b4580a54ae1f156fdc7cb09fc225090a0f7f9cc
Adnan Aziz <adnan.832@gmail.com>
2021-01-21 22:39:48 +0100

    added code for part2 of Homework

---
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -0,0 +1,15 @@
+//Goodboy-Oldboy (A dog age calculator)
+
+var dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let humanYear = dogYearFuture - dogYearOfBirth;
+const dogYear = humanYear * 7;
+const shouldShowResultInDogYears = "true";
+if 
+    (shouldShowResultInDogYears === "true") 
+    {
+    console.log("Your dog will be " + humanYear + " human years old in " + dogYearFuture);
+}
+else {
+    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);
+}
