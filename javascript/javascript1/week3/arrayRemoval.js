a1db5dddaf8a7479c5c77863b628835a5ffac08d
Adnan Aziz <adnan.832@gmail.com>
2021-02-06 01:38:15 +0100

    added code for 1st part of HomeWork

---
+++ javascript/javascript1/week3/arrayRemoval.js
@@ -0,0 +1,17 @@
+//Item array removal
+
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+
+  const nameToRemove = names.splice(1,1);
+  
+  
+  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
